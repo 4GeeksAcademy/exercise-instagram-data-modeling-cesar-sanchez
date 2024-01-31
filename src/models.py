ca3ec6d949ea01198f9c1575c47c10de5d7fb3d9
cesarsan86 <86737129+cesarsan86@users.noreply.github.com>
2024-01-31 22:12:14 +0100

    Update models.py

--- src/models.py
+++ src/models.py
@@ -41,7 +41,7 @@ class Comment(Base):
 class Post(Base):
     __tablename__ = 'post'
     id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'), nullable=False))
     user = relationship(User)
 
 class Media(Base):
